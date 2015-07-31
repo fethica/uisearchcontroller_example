bbe50572c2cf9446df6fba48dcdcb9f4c3a24f7d
Fethi El Hassasna <e.fethi.c@gmail.com>
2015-07-31 19:03:10 +0100

    Add UISearchController to the UITableViewController

--- SearchControlApp/SearchTableViewController.swift
+++ SearchControlApp/SearchTableViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class SearchTableViewController: UITableViewController {
+class SearchTableViewController: UITableViewController, UISearchResultsUpdating {
     
     let teams = ["Arsenal", "Chelsea", "Everton", "Liverpool", "Manchester City", "Manchester United", "Newcastle", "Spurs", "Swansea"]
+    var filtredTeams = [String]()
+    var resultSeachController = UISearchController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        // initiate the SeachController with an empty UISearchController
+        self.resultSeachController = UISearchController(searchResultsController: nil)
+        
+        self.resultSeachController.searchResultsUpdater = self
+        self.resultSeachController.dimsBackgroundDuringPresentation = false
+        self.resultSeachController.searchBar.sizeToFit()
+        
+        self.title = "BPL Teams"
+        
+        self.tableView.tableHeaderView = self.resultSeachController.searchBar
+        
+        self.tableView.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,66 +46,50 @@ class SearchTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
-        return self.teams.count
+        
+        // Test if resultSeachController is active to return filtred array count
+        
+        if self.resultSeachController.active {
+            return self.filtredTeams.count
+        } else {
+            return self.teams.count
+        }
+        
     }
-
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
 
-        // Configure the cell...
+        // Test if resultSeachController is active to display the filtred array Strings
         
-        cell.textLabel?.text = self.teams[indexPath.row]
-
+        if self.resultSeachController.active {
+            cell.textLabel?.text = self.filtredTeams[indexPath.row]
+        } else {
+            cell.textLabel?.text = self.teams[indexPath.row]
+        }
+        
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    // MARK: - UISearchResultsUpdating
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+        // Remove all filtredTeams items
+        self.filtredTeams.removeAll(keepCapacity: false)
+        
+        // Create a Predicate
+        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text)
+        
+        // Create NSArray (this array represent SELF in the Predicate above)
+        let array = (self.teams as NSArray).filteredArrayUsingPredicate(searchPredicate)
+        
+        // New filtredTeams from the array result
+        self.filtredTeams = array as! [String]
+        
+        // Reload TableView data
+        self.tableView.reloadData()
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
